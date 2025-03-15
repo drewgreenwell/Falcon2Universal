64f977b7a1816eac0b2447a6db93271cdfe02130
drewgreenwell <drewgreenwell@hotmail.com>
2025-03-15 02:29:09 -0700

    Updates, Cleanup, and Reorg for V2

---
+++ src/screen/app_ui.hpp
@@ -0,0 +1,132 @@
+#include "Final_Frontier_28.h"
+class AppUi {
+  LaserCommunicator *laser;
+  int16_t h = 135;
+  int16_t w = 240;
+  int xPos = 50;
+  int alarmX = 0;
+  int alarmY = h - 32;
+  int activeAlarmsLen = 2;
+  int alarmLen = 14;
+  int uiTick = 0;
+  int uiRate = 1000;
+  int alarms[14] = {1,2,4,5,6,7,8,9,10,11,12,13,14,15};
+  int activeAlarms[14] = {0,1,0,0,0,0,0,0,0,0,0,0,1,0};
+
+  public:
+    
+  int* getActiveAlarms() {
+    return activeAlarms;
+  }
+
+  AppUi(LaserCommunicator *laser) {
+    this->laser = laser;
+  }
+
+  void setup() {
+    tft.init();
+    tft.setRotation(3);
+    tft.loadFont(Final_Frontier_28);
+  }
+
+  void loop() {
+    long now = millis();
+    if(now - uiTick > uiRate){
+      uiTick = now;
+      if(!laser->hasBooted()){
+        if(laser->isWaitingToBoot()){
+          waiting_ui_update();
+        }else {
+          boot_ui_update();
+        }
+      } else {
+        ui_update();
+      }
+    }
+  }
+  
+  void prep_ui_update() {
+    tft.fillScreen(TFT_BLACK);  
+    tft.setTextWrap(false);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  }
+
+  void waiting_ui_update() {
+    prep_ui_update();
+    tft.setTextPadding(w / alarmLen / 2);
+    tft.setTextSize(2);
+    tft.drawString("Waiting to boot.", 10, (h / 2) - 10);
+  }
+
+  void boot_ui_update() {
+    prep_ui_update();
+    tft.setTextPadding(w / alarmLen / 2);
+    tft.setTextSize(2);
+    tft.drawString("Booting", 10, (h / 2) - 10);
+  }
+
+  void ui_update() {
+    // logln("pulse  " + String(TFT_WIDTH) + String(TFT_HEIGHT));
+    prep_ui_update();
+    xPos += 5;
+    if(xPos > w){
+      xPos = 0;
+    }
+    tft.setTextPadding(w / alarmLen / 2);
+    tft.setTextSize(1.25);
+    if(laser->modelName != ""){
+      tft.drawString(laser->modelName, xPos, 10);
+    } else {
+      tft.drawString("TEST", xPos, 10);
+    }
+    draw_alarm_codes();
+  }
+
+  void draw_alarm_codes() {
+    tft.setTextSize(2.75);
+    for(int i = 0; i < alarmLen; i++){
+      int x;
+      int y;
+      float half = alarmLen / 2;
+      if(i < half){
+        x = i * ( w / half) + 10;
+        y = h - 62;
+      } else {
+        x = (i - half) * (w / half) + 5;
+        y = h - 22;
+        if(alarms[i] == 9) // start of second row (single digit)
+        {
+          x += 5;
+        }
+      }
+      bool active = false;
+      if(activeAlarms[i] == 1){
+        active = true;
+      }
+      if(active) {
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+      } else {
+        tft.setTextColor(GREY, TFT_BLACK);
+      }
+      tft.drawString(String(alarms[i]), x, y);
+    }
+  }
+
+  void setMessage(const char *msg) {
+    TFT_eSprite m = TFT_eSprite(&tft);
+    m.setColorDepth(8);
+    m.createSprite(w, 24);
+    m.fillSprite(TFT_BLACK);
+
+    m.setTextFont(1);
+    m.setTextDatum(TL_DATUM);
+    m.setTextColor(TFT_WHITE);
+    m.setCursor(0, 0);
+    m.print(msg);
+
+    m.pushSprite(0, 10);
+    m.deleteSprite();
+  }
+
+
+};
--- src/screen/list.hpp
+++ src/screen/list.hpp
@@ -1,10 +1,15 @@
 class List {
+
+#ifndef MAX_LIST_SIZE
+#define MAX_LIST_SIZE 200
+#endif
+
 public:
     byte length;
-    byte data[200];
+    byte data[MAX_LIST_SIZE];
     // pass separate input data
     void append(byte item) {
-      if (length < 200) { 
+      if (length < MAX_LIST_SIZE) { 
         data[length] = item;
         length += 1;
       }
@@ -21,5 +26,13 @@ public:
         remove(i);
       }
       length = 0;
+    }
+
+    // save one list to another e.g. inputData to prevInputData
+    static void saveList(List &list, List &toList){
+      toList.clear();
+      for (byte i = 0; i < list.length; i++){
+        toList.append(list.data[i]);
+      }
     }
  };
